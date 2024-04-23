f983dd64e89be805dd7f0f174159d4253c2573a4
Fansu Janneh <fjanneh@hotmail.co.uk>
2024-04-23 09:59:53 +0100

    Transformation of data

--- datatransform.py
+++ datatransform.py
@@ -12,10 +12,6 @@ class DataTransform:
             df[col] = pd.to_numeric(df[col], errors='coerce')
         return df
 
-    def convert_month_to_categorical(df, month_column):
-        df[month_column] = df[month_column].astype('category')
-        return df
-
     def convert_weekend_to_binary(df, weekend_column):
         df[weekend_column] = df[weekend_column].astype(int)  # Convert boolean to integer directly
         return df
@@ -23,6 +19,6 @@ class DataTransform:
 
 df = pd.read_csv('customer_activity.csv')
 
-transformed_df = DataTransform.convert_to_categorical(df, ['operating_systems', 'browser', 'region', 'traffic_type', 'visitor_type'])
+transformed_df = DataTransform.convert_to_categorical(df, ['operating_systems', 'browser', 'region', 'traffic_type', 'visitor_type', 'month'])
 transformed_df = DataTransform.convert_to_numeric(df, ['administrative_duration', 'informational_duration', 'product_related_duration', 'bounce_rates', 'exit_rates', 'page_values'])
-transformed_df = DataTransform.convert_month_to_categorical(df, 'month')
+transformed_df = DataTransform.convert_weekend_to_binary(df, 'weekend')
